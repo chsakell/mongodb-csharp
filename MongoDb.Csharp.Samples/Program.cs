b9c16a0637eb3e1dbf327e2feb6455273cf27c70
chsakell <chsakell@gmail.com>
2020-02-28 23:36:47 +0200

    Add project files.

---
+++ MongoDb.Csharp.Samples/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MongoDb.Csharp.Samples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
