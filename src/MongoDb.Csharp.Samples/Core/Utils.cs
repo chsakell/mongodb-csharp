a8cf7add5e2e13af1e8d7fe414eb883d84d8885e
chsakell <chsakell@gmail.com>
2020-03-02 13:17:24 +0200

    switch to async operations, break samples

--- src/MongoDb.Csharp.Samples/Core/Utils.cs
+++ src/MongoDb.Csharp.Samples/Core/Utils.cs
@@ -13,17 +13,28 @@ namespace MongoDb.Csharp.Samples.Core
     public class Utils
     {
         public static string DefaultConnectionString = "mongodb://localhost:27017";
+        private static string EmptySpace = "                     ";
 
         public static void Log(BsonDocument doc)
         {
             Debug.WriteLine(JsonConvert.SerializeObject(BsonTypeMapper.MapToDotNetValue(doc)));
         }
 
-        public static void Log(IEnumerable<BsonDocument> docs)
+        public static void Log(IEnumerable<BsonDocument> docs, string message = null)
         {
+            if (!string.IsNullOrEmpty(message))
+            {
+                Debug.WriteLine($"{EmptySpace}{message}");
+            }
             foreach (var doc in docs)
             {
                 Log(doc);
+            }
+
+            if (!string.IsNullOrEmpty(message))
+            {
+                Debug.WriteLine($"{EmptySpace}{message}");
+                Debug.WriteLine(string.Empty);
             }
         }
 
