241ba86be306728c848476d809cce444ae7f9986
chsakell <chsakell@gmail.com>
2020-03-10 23:29:37 +0200

    minor changes

--- src/MongoDb.Csharp.Samples/Crud/Read/Query/ElementOperators.cs
+++ src/MongoDb.Csharp.Samples/Crud/Read/Query/ElementOperators.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 using MongoDB.Bson;
 using MongoDb.Csharp.Samples.Core;
 using MongoDb.Csharp.Samples.Models;
