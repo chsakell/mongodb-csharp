84a66e5a92bf0a94351339d2cf59c8b943eb4479
chsakell <chsakell@gmail.com>
2020-03-16 20:28:49 +0200

    minor

--- src/MongoDb.Csharp.Samples/Crud/Update/UpdatingArrays.cs
+++ src/MongoDb.Csharp.Samples/Crud/Update/UpdatingArrays.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 using MongoDB.Bson;
 using MongoDb.Csharp.Samples.Core;
 using MongoDb.Csharp.Samples.Models;
